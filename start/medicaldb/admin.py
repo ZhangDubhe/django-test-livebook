0f20cdb746a02b9c6bbe307448c16ad6def1cb0d
Dubhee <zht_dubhe@163.com>
2018-01-25 17:06:16 -0700

    删除start项目，django迁移sqlite存在无法更新表格名的情况

--- start/medicaldb/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Disease, Symptom, Property, Value
-admin.site.register(Disease)
-admin.site.register(Symptom)
-admin.site.register(Value)
-admin.site.register(Property)
