07aa7276f79dbccf718016c551d853f0e86d8dbe
Dubhee <zht_dubhe@163.com>
2018-03-08 14:47:20 -0700

    initial database

--- data/query.sql
+++ data/query.sql
@@ -1,4 +1,6 @@
-select  c.CUI, "symptom",c.STR from mrconso c, mrsty s where c.CUI = s.CUI and s.TUI = "T184"
-;
-select c.STR, c.CUI from mrconso c, mrsty s where c.CUI = s.CUI and s.TUI = "T047"
-;
+select c.cui 'CUI' , c.str 'Name', s.sty 'SemanticType', d.DEF 'Definition' , c.SAB 'Source' from mrconso c , mrsty s , mrdef d where c.str like '%otitis%' and s.tui = 'T047' and c.cui = s.cui and c.cui = d.cui and d.def is not NULL and c.SAB = d.SAB and c.SAB = 'NCI' group by c.cui;
+
+
+select c.cui 'CUI' , c.str 'Name', s.sty 'SemanticType', d.DEF 'Definition' , c.SAB 'Source' from mrconso c , mrsty s , mrdef d where s.tui = 'T184' and c.cui = s.cui and c.cui = d.cui and d.def is not NULL and c.SAB = d.SAB and c.SAB = 'NCI' group by c.cui;
+
+select c.cui 'CUI' , c.str 'Name', s.sty 'SemanticType', d.DEF 'Definition' , c.SAB 'Source' from mrconso c , mrsty s , mrdef d where (s.tui = 'T032' or s.tui= 'T080' )and c.cui = s.cui and c.cui = d.cui and d.def is not NULL and c.SAB = d.SAB and c.SAB = 'NCI' group by c.cui;
