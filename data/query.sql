5e6a508e8d7f7fede5e91a5205c228a43a4c0556
ZhangDubhe <zht_dubhe@163.com>
2018-04-12 18:16:44 -0600

    Import disease and delete the same row.

--- data/query.sql
+++ data/query.sql
@@ -9,3 +9,7 @@ select c.cui 'CUI' , c.str 'Name', s.sty 'SemanticType', d.DEF 'Definition' , c.
 select c.cui 'CUI' , c.str 'Name', s.sty 'SemanticType', d.DEF 'Definition' , r.CUI2 "t_CUI", (select str 'Topic' from mrconso where cui = t_CUI), c.SAB 'Source' from mrconso c , mrsty s , mrdef d, mrrel r where c.str like '%otitis%' and s.tui = 'T047' and c.cui = s.cui and c.cui = d.cui and d.def is not NULL and c.SAB = d.SAB and c.SAB = 'NCI' group by c.cui;
 
 select c.cui 'CUI' , c.str 'Name', s.sty 'SemanticType', d.DEF 'Definition' , c.SAB 'Source' from mrconso c , mrsty s , mrdef d where s.tui = 'T047' and c.cui = s.cui and c.cui = d.cui and d.def is not NULL and c.SAB = d.SAB and c.SAB = 'NCI' group by c.cui;
+
+select * from demo_disease group by content_unique_id having
+count(*)>1;
+delete from demo_disease where demo_disease.id not in (select MAX(demo_disease.id) from demo_disease group by content_unique_id);
